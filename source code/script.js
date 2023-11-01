a853ca910b7b1928eb0981c9a4e7ff6f753cfe4f
nikhil <nikhilgupta7104@gmail.com>
2023-11-01 21:32:01 +0530

    frontend done

---
+++ source code/script.js
@@ -0,0 +1,14 @@
+let audelement = new Audio("/Spotify/resources/songs/1.mp3");
+
+let songs = [
+    {songname:"song name",filepath:"/Spotify/resources/songs/1.mp3",coverpath:"/Spotify/resources/covers/1.jpg"},,
+    {songname:"song name",filepath:"/Spotify/resources/songs/1.mp3",coverpath:"/Spotify/resources/covers/1.jpg"},
+    {songname:"song name",filepath:"/Spotify/resources/songs/1.mp3",coverpath:"/Spotify/resources/covers/1.jpg"},
+    {songname:"song name",filepath:"/Spotify/resources/songs/1.mp3",coverpath:"/Spotify/resources/covers/1.jpg"},
+    {songname:"song name",filepath:"/Spotify/resources/songs/1.mp3",coverpath:"/Spotify/resources/covers/1.jpg"},
+    {songname:"song name",filepath:"/Spotify/resources/songs/1.mp3",coverpath:"/Spotify/resources/covers/1.jpg"},
+    {songname:"song name",filepath:"/Spotify/resources/songs/1.mp3",coverpath:"/Spotify/resources/covers/1.jpg"},
+    {songname:"song name",filepath:"/Spotify/resources/songs/1.mp3",coverpath:"/Spotify/resources/covers/1.jpg"},
+    {songname:"song name",filepath:"/Spotify/resources/songs/1.mp3",coverpath:"/Spotify/resources/covers/1.jpg"},
+    {songname:"song name",filepath:"/Spotify/resources/songs/1.mp3",coverpath:"/Spotify/resources/covers/1.jpg"}
+]
