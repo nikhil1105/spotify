abcc8aea06258b140b2b80258e286910ff5fd545
nikhil <nikhilgupta7104@gmail.com>
2023-11-03 19:38:16 +0530

    project completed

--- source code/script.js
+++ source code/script.js
@@ -1,39 +1,36 @@
 let songs = [
-    { songname: "Bohemian Rhapsody", filepath: "/Spotify/resources/songs/1.mp3", coverpath: "/Spotify/resources/covers/1.jpg" },
-    { songname: "Smells Like Teen Spirit", filepath: "/Spotify/resources/songs/2.mp3", coverpath: "/Spotify/resources/covers/2.jpg" },
-    { songname: "Imagine", filepath: "/Spotify/resources/songs/3.mp3", coverpath: "/Spotify/resources/covers/3.jpg" },
-    { songname: "Billie Jean", filepath: "/Spotify/resources/songs/4.mp3", coverpath: "/Spotify/resources/covers/4.jpg" },
-    { songname: "Like a Rolling Stone", filepath: "/Spotify/resources/songs/5.mp3", coverpath: "/Spotify/resources/covers/5.jpg" },
-    { songname: "Hey Jude", filepath: "/Spotify/resources/songs/6.mp3", coverpath: "/Spotify/resources/covers/6.jpg" },
-    { songname: "I Will Always Love You", filepath: "/Spotify/resources/songs/7.mp3", coverpath: "/Spotify/resources/covers/7.jpg" },
-    { songname: "Hallelujah", filepath: "/Spotify/resources/songs/8.mp3", coverpath: "/Spotify/resources/covers/8.jpg" },
-    { songname: "Shape of You", filepath: "/Spotify/resources/songs/9.mp3", coverpath: "/Spotify/resources/covers/9.jpg" },
-    { songname: "Gangnam Style", filepath: "/Spotify/resources/songs/10.mp3", coverpath: "/Spotify/resources/covers/10.jpg" }
-]
+    { songname: "Believer", filepath: "/Spotify/resources/songs/1.mp3", coverpath: "/Spotify/resources/covers/1.jpg" },
+    { songname: "Thunder", filepath: "/Spotify/resources/songs/2.mp3", coverpath: "/Spotify/resources/covers/2.jpg" },
+    { songname: "Girls Like You", filepath: "/Spotify/resources/songs/3.mp3", coverpath: "/Spotify/resources/covers/3.jpg" },
+    { songname: "Shape of You", filepath: "/Spotify/resources/songs/4.mp3", coverpath: "/Spotify/resources/covers/4.jpg" },
+    { songname: "Perfect", filepath: "/Spotify/resources/songs/5.mp3", coverpath: "/Spotify/resources/covers/5.jpg" },
+    { songname: "Baby", filepath: "/Spotify/resources/songs/6.mp3", coverpath: "/Spotify/resources/covers/6.jpg" },
+    { songname: "Beautiful Mistakes", filepath: "/Spotify/resources/songs/7.mp3", coverpath: "/Spotify/resources/covers/7.jpg" },
+    { songname: "Memories", filepath: "/Spotify/resources/songs/8.mp3", coverpath: "/Spotify/resources/covers/8.jpg" }
 
+]
 const slistmain = document.getElementById("songlistmain");
 let i = 1;
 
 songs.forEach(element => {
+
     const sl = document.createElement("div");
     sl.classList.add("songlist");
     const img = document.createElement("img");
     img.src = element.coverpath;
     img.alt = "";
     const sname = document.createElement("span");
+    sname.id = "songname" + i;
     sname.innerHTML = element.songname;
 
     const splaylist = document.createElement("span")
     splaylist.classList.add("songlistplay");
 
-    const tstamp = document.createElement("span");
-    tstamp.innerHTML = "05.33";
 
 
     const icon = document.createElement("i");
     icon.classList.add("fa-regular", "songitemplay", "fa-circle-play");
     icon.id = i++;
-    splaylist.appendChild(tstamp);
     splaylist.appendChild(icon);
     sl.appendChild(img);
     sl.appendChild(sname);
@@ -49,6 +46,7 @@ const next = document.getElementById("next");
 
 const bar = document.getElementById("bar");
 let songnumber = 0;
+let songstart = false;
 
 playbutton.addEventListener("click", () => {
     if (audelement.paused || audelement.currentTime <= 0) {
@@ -56,14 +54,27 @@ playbutton.addEventListener("click", () => {
         playbutton.classList.remove("fa-circle-play");
         playbutton.classList.add("fa-circle-pause");
         gif.style.opacity = 1;
+        control();
 
     } else {
         audelement.pause();
         playbutton.classList.remove("fa-circle-pause");
         playbutton.classList.add("fa-circle-play");
         gif.style.opacity = 0;
+        Array.from(document.getElementsByClassName('songitemplay')).forEach((element) => {
+
+            if (parseInt(audelement.src.slice(-5).charAt(0)) == parseInt(element.id)) {
+                makeAllPlays();
+                element.classList.remove("fa-circle-pause");
+                element.classList.add("fa-circle-play");
+                document.getElementById("sname").innerHTML = document.getElementById("songname" + parseInt(element.id)).innerHTML;
+            }
+
+        })
+
 
     }
+
 })
 
 
@@ -74,31 +85,53 @@ bar.addEventListener("change", () => {
 
 
 back.addEventListener("click", () => {
-    if (songnumber > 0) {
-        songnumber = parseInt(songnumber);
-        songnumber--;
+    if (songstart==false) {
+        songnumber++;
+        songstart=true;
+    }
+    songnumber--;
+    if (songnumber < 0) {
+        songnumber = songs.length - 1;
         audelement.src = songs[parseInt(songnumber)].filepath;
         audelement.currentTime = 0;
         audelement.play();
         playbutton.classList.remove("fa-circle-play");
         playbutton.classList.add("fa-circle-pause");
         gif.style.opacity = 1;
+        
+
     }
-    else {
-        songnumber = parseInt(songnumber);
+    else if (songnumber == 0) {
+        audelement.src = songs[parseInt(songnumber)].filepath;
+        audelement.currentTime = 0;
         audelement.play();
         playbutton.classList.remove("fa-circle-play");
         playbutton.classList.add("fa-circle-pause");
         gif.style.opacity = 1;
+        
+    }
+    else {
 
+        audelement.src = songs[parseInt(songnumber)].filepath;
+        audelement.currentTime = 0;
+        audelement.play();
+        playbutton.classList.remove("fa-circle-play");
+        playbutton.classList.add("fa-circle-pause");
+        gif.style.opacity = 1;
+        
     }
 
+    control();
 })
 
 
 next.addEventListener("click", () => {
-    if (songnumber == songs.length - 1) {
-        songnumber = parseInt(songnumber);
+    if (songstart==false) {
+        songnumber--;
+        songstart=true;
+    }
+    songnumber++;
+    if (songnumber == songs.length) {
         songnumber = 0;
         audelement.src = songs[parseInt(songnumber)].filepath;
         audelement.currentTime = 0;
@@ -109,33 +142,44 @@ next.addEventListener("click", () => {
 
 
     } else if (songnumber == 0) {
-
-        audelement.play();
-        playbutton.classList.remove("fa-circle-play");
-        playbutton.classList.add("fa-circle-pause");
-        gif.style.opacity = 1;
-        songnumber = 0.1;
-
-
-    }
-    else {
-        songnumber = parseInt(songnumber);
-
-        songnumber++;
         audelement.src = songs[parseInt(songnumber)].filepath;
         audelement.currentTime = 0;
         audelement.play();
         playbutton.classList.remove("fa-circle-play");
         playbutton.classList.add("fa-circle-pause");
         gif.style.opacity = 1;
+    }
+
+
+    else {
+        audelement.src = songs[parseInt(songnumber)].filepath;
+        audelement.currentTime = 0;
+        audelement.play();
+        playbutton.classList.remove("fa-circle-play");
+        playbutton.classList.add("fa-circle-pause");
+        gif.style.opacity = 1;
+        
+
 
 
     }
+    control();
 })
 
 audelement.addEventListener('timeupdate', () => {
     let progress = parseInt((audelement.currentTime / audelement.duration) * 100);
     bar.value = progress;
+    if (audelement.currentTime == audelement.duration) {
+        if (songstart == true) {
+            songstart = false;
+            songnumber++;
+        }
+        songnumber++;
+        audelement.src = "/Spotify/resources/songs/" + parseInt(songnumber) + ".mp3";
+        audelement.currentTime = 0;
+        audelement.play();
+        gif.style.opacity = 1;
+    }
 })
 
 const makeAllPlays = () => {
@@ -147,26 +191,46 @@ const makeAllPlays = () => {
 
 Array.from(document.getElementsByClassName('songitemplay')).forEach((element) => {
     element.addEventListener("click", (ele) => {
-        if (audelement.played && audelement.src.slice(-5)==(element.id+".mp3" ) && !(audelement.paused) ) {
+        if (audelement.played && audelement.src.slice(-5) == (element.id + ".mp3") && !(audelement.paused)) {
             element.classList.remove("fa-circle-pause");
             element.classList.add("fa-circle-play");
+            playbutton.classList.remove("fa-circle-pause");
             playbutton.classList.add("fa-circle-play");
             audelement.pause();
             gif.style.opacity = 0;
 
+
         } else {
-            console.log("in else ",audelement.played,audelement.src);
             makeAllPlays();
             element.classList.remove("fa-circle-play");
             element.classList.add("fa-circle-pause");
+            playbutton.classList.remove("fa-circle-play");
             playbutton.classList.add("fa-circle-pause");
             audelement.src = "/Spotify/resources/songs/" + parseInt(element.id) + ".mp3";
             audelement.currentTime = 0;
             audelement.play();
             gif.style.opacity = 1;
         }
-
+        document.getElementById("sname").innerText = songs[parseInt(element.id - 1)].songname;
 
     })
 })
+
+const control = () => {
+
+
+    Array.from(document.getElementsByClassName('songitemplay')).forEach((element) => {
+
+        if (parseInt(audelement.src.slice(-5).charAt(0))==(element.id)) {
+            makeAllPlays();
+            element.classList.remove("fa-circle-play");
+            element.classList.add("fa-circle-pause");
+            document.getElementById("sname").innerText = songs[parseInt(element.id - 1)].songname;
+        }
+
+    })
+
+
+
+}
 
